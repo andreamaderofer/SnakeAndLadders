6dda61c4dc2a48ce9b266339ee724d00e20fba06
Andrea <Andrea@Andrea-Laptop>
2016-11-15 11:12:25 +0100

    bh

--- SnakeAndLadders/src/Feld.java
+++ SnakeAndLadders/src/Feld.java
@@ -1,23 +1,24 @@
 
 public class Feld {
-	
-	int x;
-	int y; 
-	
-	public void setX(){
-		
+
+	private int x;
+	private int y;
+
+	public void setX() {
+	}
+
+	public void setY() {
 	}
 	
-	public void setY(){
+	public void setPosition(){
 		
 	}
-	public int getX(){
+
+	public int getX() {
 		return this.x;
-		
 	}
-	
-	public int getY(){
+
+	public int getY() {
 		return this.y;
-		
 	}
 }
