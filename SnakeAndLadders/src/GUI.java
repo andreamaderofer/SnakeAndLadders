13d72dcc9aeebe5d1cb836c20d56f5086c5a3e7d
Andrea <Andrea@Andrea-Laptop>
2016-11-30 08:01:04 +0100

    klassen

--- SnakeAndLadders/src/GUI.java
+++ SnakeAndLadders/src/GUI.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
 
@@ -8,31 +9,48 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 	private SpielfeldEinlesen se = new SpielfeldEinlesen();
-
+	
 	public GUI(int i) {
 
 		se.einlesen(i);
 		erstellen(se.getSpielfeld());
+		repaint();
 	}
-
+	
+    public class MyDraw extends JPanel {
+        public void paintComponent(Graphics g) {
+           super.paintComponent(g);
+           g.drawLine(0, 0, 200, 200);
+        }
+    }
+	
 	public void erstellen(String[][] spielfeld) {
-		JPanel[][] labelfeld = new JPanel[324][324];
-		for (int i = 0; i < spielfeld.length; i++) {
-			for (int a = 0; a < spielfeld.length; a++) {
 
+		JFrame frame = new JFrame();
+		frame.getContentPane();
+		frame.setSize(500, 500);
+		frame.getContentPane().setBounds(MAXIMIZED_BOTH, MAXIMIZED_BOTH, MAXIMIZED_HORIZ, MAXIMIZED_VERT);
+		GridLayout gri=new GridLayout(10,10);
+		frame.getContentPane().setLayout(gri);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.orange);
+		int feld=1;
+		JPanel[][] labelfeld = new JPanel[spielfeld.length][spielfeld.length];
+		for (int i = 100; i >=0; i--) {
+			for (int a = 0; a < spielfeld.length; a++) {
+				
 				if (spielfeld[i][a] == null) {
 					break;
 				}
-				// System.out.println(spielfeld[i][a]);
+
 				if (spielfeld[i][a].equals("BB")) {
 					labelfeld[i][a] = new JPanel();
-					labelfeld[i][a].setToolTipText("HALLO");
-					labelfeld[i][a].setBackground(Color.blue);
-					ImageIcon im=new ImageIcon("schlangen1.png");
-					Image imag=im.getImage();
-					Image ima=imag.getScaledInstance(50, 50,  java.awt.Image.SCALE_SMOOTH);  
+					JLabel lab=new JLabel(""+feld);
+					labelfeld[i][a].add(lab);
+					labelfeld[i][a].setBackground(Color.blue); 
 					labelfeld[i][a].setSize(50,50);
-					labelfeld[i][a].add(new JLabel(new ImageIcon(ima)));
+					frame.getContentPane().add(labelfeld[i][a].add(new MyDraw()));					
+					feld++;
 					continue;
 				}
 				if (spielfeld[i][a].equals("BW")) {
@@ -49,36 +67,36 @@ public class GUI extends JFrame {
 				}
 				if (spielfeld[i][a].equals("AA")) {
 					labelfeld[i][a] = new JPanel();
-					labelfeld[i][a].setToolTipText("HALLO");
-					labelfeld[i][a].setBackground(Color.green);
+					JLabel lab=new JLabel(""+feld);
+					labelfeld[i][a].add(lab);
+					labelfeld[i][a].setBackground(Color.LIGHT_GRAY);
 					labelfeld[i][a].setSize(50,50);
+					frame.getContentPane().add(labelfeld[i][a]);
+					feld++;
 					continue;
 				} else {
 					labelfeld[i][a] = new JPanel();
+					feld++;
 					continue;
 				}
 			}
 		}
-
-		JFrame frame = new JFrame();
-		frame.getContentPane();
-		frame.setSize(500, 500);
-		frame.getContentPane().setBounds(MAXIMIZED_BOTH, MAXIMIZED_BOTH, MAXIMIZED_HORIZ, MAXIMIZED_VERT);
-		GridLayout gri=new GridLayout(10,10);
-		frame.getContentPane().setLayout(gri);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		frame.getContentPane().setBackground(Color.orange);
 		
-		for (int i = 0; i < labelfeld.length; i++) {
+		/*for (int i = 0; i < labelfeld.length; i++) {
 			for (int a = 0; a < labelfeld.length; a++) {
 				if (labelfeld[i][a] == null) {
 					break;
 				} else {
-					frame.getContentPane().add(labelfeld[i][a]);
+					if(i%5==0 && a%5==0){
+						frame.getContentPane().add(labelfeld[i][a].add(new MyDraw()));
+					}else{
+						frame.getContentPane().add(labelfeld[i][a]);						
+					}
 				}
 			}
-		}
+		}*/
 		frame.setVisible(true);
+		repaint();
 	}
 
 	public void aktualisieren() {
